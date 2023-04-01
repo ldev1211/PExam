c498f16c1ff2027ea49f48949b955a0180dff7d8
Admin <luongdien1211@gmail.com>
2023-04-01 08:30:38 +0700

    1/4

--- app/src/main/java/ldev/ptithcm/pexam/application/ApplicationConfig.java
+++ app/src/main/java/ldev/ptithcm/pexam/application/ApplicationConfig.java
@@ -10,8 +10,7 @@ import io.socket.client.Socket;
 public class ApplicationConfig extends Application {
     public static int numAnswered = 0;
     public static String NAME_SQLITE = "PExamSQLite.sqlite";
-    public static String baseURLPExamServer = "http://113.161.104.213:3001";
-//    public static String baseURLPExamServer = "http://192.168.1.9:4002";
+    public static String baseURLPExamServer = "http://apildevmobile.com";
     public static Socket mSocket;
     static {
         try {
